379ce2dd8c6cc310e8b2716b4b828d73eea77568
peegonator <156694576+peegonator@users.noreply.github.com>
2024-04-07 12:14:19 -0700

    Delete configure.py

--- configure.py
+++
@@ -1,7 +0,0 @@
-def configure_settings():
-    # Prompt user for SSH IP address
-    ssh_ip = input("Enter the SSH IP address: ")
-
-    # Start SSH server with user-provided IP
-    start_ssh_server(ssh_ip)
-
